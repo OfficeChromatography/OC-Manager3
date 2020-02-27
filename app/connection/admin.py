3f889c319b3031e64b6c9ebe32aedb9e66f3d5ae
lucassing <lucassinggg@gmail.com>
2020-02-27 14:05:33 +0100

    Fix the admin view

--- app/connection/admin.py
+++ app/connection/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import Connection_Db
-
+from django.forms import Textarea
+from django.db import models
 
 class Connection_DbAdmin(admin.ModelAdmin):
-    readonly_fields = ('oc_lab', 'baudrate', 'timeout', 'time_of_connection',)
+    fields = ('id','oc_lab', 'username','baudrate', 'timeout', 'time_of_connection', 'chattext')
+    readonly_fields = ('id','oc_lab', 'username','baudrate', 'timeout', 'time_of_connection')
+    formfield_overrides = {models.TextField: dict(widget=Textarea(attrs=dict(readonly=True, cols=200, rows=20)))}
 
 
 admin.site.register(Connection_Db, Connection_DbAdmin)
