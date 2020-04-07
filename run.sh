693ef021d7383cba5e1a1c7ee8cf01a4c0313351
lucassing <lucassinggg@gmail.com>
2020-04-07 14:30:53 +0200

    add build for every run

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 DIRECTORY=`dirname $0`
 echo $DIRECTORY
 cd $DIRECTORY
+sudo docker rmi oc-image:octag -f
 sudo docker-compose down
 sudo docker-compose run app sh -c "python manage.py makemigrations"
 sudo docker-compose run app sh -c "python manage.py migrate"
