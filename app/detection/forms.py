d25519a664ccb05fd58f3e9dac5825b8e67d28e2
lucassing <lucassinggg@gmail.com>
2020-04-19 17:43:58 +0200

    migrated to v4l2

--- app/detection/forms.py
+++ app/detection/forms.py
@@ -16,19 +16,34 @@ AWB_MODES = (('0', 'off'),
             ('7', 'incandescent'),
             ('8', 'flash'),
             ('9', 'horizon'))
-EXPOSURE_MODES = (('0', 'off'),
-                ('1', 'auto'),
-                ('2', 'night'),
-                ('3', 'nightpreview'),
-                ('4', 'backlight'),
-                ('5', 'spotlight'),
-                ('6', 'sports'),
-                ('7', 'snow'),
-                ('8', 'beach'),
-                ('9', 'verylong'),
-                ('10', 'fixedfps'),
-                ('11', 'antishake'),
-                ('12', 'fireworks'))
+
+AUTO_EXPOSURE = (('0', 'off'),
+                ('1', 'auto'))
+
+ISO_SENSITIVITY =   ((0,0),
+                    (1,100000),
+                    (2,200000),
+                    (3,400000),
+                    (4,900000))
+
+ISO_SENSITIVITY_AUTO =  ((0,'Manual'),
+                        (1,'Auto'))
+FORMATS = ((0,'YU12'),    # (Planar YUV 4:2:0)
+            (1,'YUYV'),    # (YUYV 4:2:2)
+            (2,'RGB3'),    # (24-bit RGB 8-8-8)
+            (3,'JPEG'),    # (JFIF JPEG, compressed)
+            (4,'H264'),    # (H.264, compressed)
+            (5,'MJPG'),    # (Motion-JPEG, compressed)
+            (6,'YVYU'),    # (YVYU 4:2:2)
+            (7,'VYUY'),    # (VYUY 4:2:2)
+            (8,'UYVY'),    # (UYVY 4:2:2)
+            (9,'NV12'),    # (Y/CbCr 4:2:0)
+            (10,'BGR3'),   # (24-bit BGR 8-8-8)
+            (11,'YV12'),   # (Planar YVU 4:2:0)
+            (12,'NV21'),   # (Y/CrCb 4:2:0)
+            (13,'BGR4'))   # (32-bit BGRA/X 8-8-8-8))
+
+
 IMAGE_EFFECTS = (('0', 'none'),
                 ('1', 'negative'),
                 ('2', 'solarize'),
@@ -60,6 +75,7 @@ ISO = (('0','Auto'),
 ('5','500'),
 ('6','640'),
 ('7','800'))
+
 ROTATION = (('0','0'),
 ('1','90'),
 ('2','180'),
@@ -73,18 +89,19 @@ class ShootConfigurationForm(forms.Form):
                    #   saturation 0x00980902 (int)    : min=-100 max=100 step=1 default=0 value=0 flags=slider
                    #  red_balance 0x0098090e (int)    : min=1 max=7999 step=1 default=1000 value=1000 flags=slider
                    # blue_balance 0x0098090f (int)    : min=1 max=7999 step=1 default=1000 value=1000 flags=slider
+                   # sharpness 0x0098091b (int)    : min=-100 max=100 step=1 default=0 value=0 flags=slider
 
     #             horizontal_flip 0x00980914 (bool)   : default=0 value=0
     #               vertical_flip 0x00980915 (bool)   : default=0 value=0
     #        power_line_frequency 0x00980918 (menu)   : min=0 max=3 default=1 value=1
-    #                   sharpness 0x0098091b (int)    : min=-100 max=100 step=1 default=0 value=0 flags=slider
+
     #               color_effects 0x0098091f (menu)   : min=0 max=15 default=0 value=0
     #                      rotate 0x00980922 (int)    : min=0 max=360 step=90 default=0 value=0 flags=modify-layout
     #          color_effects_cbcr 0x0098092a (int)    : min=0 max=65535 step=1 default=32896 value=32896
 
 
-# USER CONTROLS:
 
+# USER CONTROLS:
     brightness = forms.DecimalField(label='Brightness',
                             required=False,
                             max_digits=3,
@@ -101,7 +118,6 @@ class ShootConfigurationForm(forms.Form):
                             min_value=-100,
                             widget=forms.NumberInput(attrs={'size': '9', 'placeholder':'0', 'class':'form-control'}))
 
-    # saturation = -100~100  default 0
     saturation = forms.DecimalField(label='Saturation',
                             required=False,
                             max_digits=3,
@@ -126,6 +142,88 @@ class ShootConfigurationForm(forms.Form):
                             min_value=1,
                             widget=forms.NumberInput(attrs={'size': '9', 'placeholder':'1000', 'class':'form-control'}))
 
+    sharpness = forms.DecimalField(label='Sharpness',
+                            required=False,
+                            max_digits=3,
+                            decimal_places=0,
+                            max_value=100,
+                            min_value=-100,
+                            widget=forms.NumberInput(attrs={'size': '9', 'placeholder':'0', 'class':'form-control'}))
+
+# Camera Controls
+#
+            #                   auto_exposure 0x009a0901 (menu)   : min=0 max=3 default=0 value=0
+            #          exposure_time_absolute 0x009a0902 (int)    : min=1 max=10000 step=1 default=1000 value=1000
+#      exposure_dynamic_framerate 0x009a0903 (bool)   : default=0 value=0
+#              auto_exposure_bias 0x009a0913 (intmenu): min=0 max=24 default=12 value=12
+#       white_balance_auto_preset 0x009a0914 (menu)   : min=0 max=10 default=1 value=1
+#             image_stabilization 0x009a0916 (bool)   : default=0 value=0
+    #                 iso_sensitivity 0x009a0917 (intmenu): min=0 max=4 default=0 value=0
+    #            iso_sensitivity_auto 0x009a0918 (menu)   : min=0 max=1 default=1 value=1
+#          exposure_metering_mode 0x009a0919 (menu)   : min=0 max=2 default=0 value=0
+#                      scene_mode 0x009a091a (menu)   : min=0 max=13 default=0 value=0
+
+
+
+# Camera Controls
+
+    auto_exposure = forms.ChoiceField(label='Auto Exposure', choices = AUTO_EXPOSURE, widget=forms.Select(attrs={'class':'form-control'}))
+
+    exposure_time_absolute = forms.DecimalField(label='Absolute Exposure Time',
+                            required=False,
+                            max_digits=5,
+                            decimal_places=0,
+                            max_value=10000,
+                            min_value=1,
+                            widget=forms.NumberInput(attrs={'size': '9', 'placeholder':'50', 'class':'form-control'}))
+
+    iso_sensitivity = forms.ChoiceField(label='ISO Sensitivity', choices = ISO_SENSITIVITY, widget=forms.Select(attrs={'class':'form-control'}))
+
+    iso_sensitivity_auto = forms.ChoiceField(label='ISO Sensitivity auto', choices = ISO_SENSITIVITY_AUTO, widget=forms.Select(attrs={'class':'form-control'}))
+
+# Frame_Controls
+
+    resolution = forms.CharField(label='Resolution',
+                    required=False,
+                    max_length = 9,
+                    widget=forms.TextInput(
+                                        attrs={
+                                                'size': '9',
+                                                'placeholder':'WIDTHxHEIGHT',
+                                                'class':'form-control',
+                                                'data-toggle':"tooltip",
+                                                'data-placement':'top',
+                                                'title':"def. 1024x1024",
+                                                }
+                                            )
+                                        )
+
+    framerate = forms.DecimalField(label='Framerate',
+                    max_digits=3,
+                    required=False,
+                    decimal_places=1,
+                    max_value=90,
+                    min_value=1,
+                    widget=forms.NumberInput(
+                                        attrs={
+                                                'class':'form-control',
+                                                'data-toggle':'tooltip',
+                                                'data-placement':'top',
+                                                'placeholder':'1.0',
+                                                }
+                                            )
+                                        )
+
+    pixelformat = forms.ChoiceField(label='Formats', choices = FORMATS, widget=forms.Select(attrs={'class':'form-control'}))
+
+
+
+
+
+
+
+
+
     # hue = forms.DecimalField(label='HUE',
     #                         required=False,
     #                         max_digits=4,
@@ -149,30 +247,6 @@ class ShootConfigurationForm(forms.Form):
     #                         max_value=1000,
     #                         min_value=-1000,
     #                         widget=forms.NumberInput(attrs={'size': '9', 'placeholder':'50', 'class':'form-control'}))
-
-
-    # def clean_resolution(self):
-    #     resolution = self.cleaned_data['resolution']
-    #     if resolution:
-    #         resolution = resolution.casefold()
-    #         if not 'x':
-    #             raise forms.ValidationError("Invalid Format.")
-    #         else:
-    #             position = resolution.find('x')
-    #             width = int(resolution[:position])
-    #             height = int(resolution[position+1:])
-    #     else:
-    #         width = 640
-    #         height = 480
-    #     return [width, height]
-    #
-    # def clean_framerate(self):
-    #     framerate = self.cleaned_data['framerate']
-    #     if framerate:
-    #         framerate = float(framerate)
-    #     if not framerate:
-    #         framerate = 1.0
-    #     return framerate
     #
     # def clean_shutter_speed(self):
     #     shutter_speed = self.cleaned_data['shutter_speed']
@@ -205,7 +279,7 @@ class ShootConfigurationForm(forms.Form):
         if saturation:
             saturation = int(saturation)
         if not saturation:
-            saturation = 50
+            saturation = 0
         return saturation
 
     def clean_red_balance(self):
@@ -217,12 +291,82 @@ class ShootConfigurationForm(forms.Form):
         return red_balance
 
     def clean_blue_balance(self):
-        blue_balance = self.cleaned_data['red_balance']
+        blue_balance = self.cleaned_data['blue_balance']
         if blue_balance:
             blue_balance = int(blue_balance)
         if not blue_balance:
             blue_balance = 1000
         return blue_balance
+
+    def clean_sharpness(self):
+        sharpness = self.cleaned_data['sharpness']
+        if sharpness:
+            sharpness = int(sharpness)
+        if not sharpness:
+            sharpness = 0
+        return sharpness
+
+    def clean_auto_exposure(self):
+        auto_exposure = self.cleaned_data['auto_exposure']
+        if auto_exposure:
+            auto_exposure = int(auto_exposure)
+        if not auto_exposure:
+            auto_exposure = 0
+        return auto_exposure
+
+    def clean_exposure_time_absolute(self):
+        exposure_time_absolute = self.cleaned_data['exposure_time_absolute']
+        if exposure_time_absolute:
+            exposure_time_absolute = int(exposure_time_absolute)
+        if not exposure_time_absolute:
+            exposure_time_absolute = 1000
+        return exposure_time_absolute
+
+    def clean_iso_sensitivity(self):
+        iso_sensitivity = self.cleaned_data['iso_sensitivity']
+        if iso_sensitivity:
+            iso_sensitivity = int(iso_sensitivity)
+        if not iso_sensitivity:
+            iso_sensitivity = 0
+        return iso_sensitivity
+
+    def clean_iso_sensitivity_auto(self):
+        iso_sensitivity_auto = self.cleaned_data['iso_sensitivity_auto']
+        if iso_sensitivity_auto:
+            iso_sensitivity_auto = int(iso_sensitivity_auto)
+        if not iso_sensitivity_auto:
+            iso_sensitivity_auto = 0
+        return iso_sensitivity_auto
+
+    def clean_resolution(self):
+        resolution = self.cleaned_data['resolution']
+        if resolution:
+            resolution = resolution.casefold()
+            if not 'x':
+                raise forms.ValidationError("Invalid Format.")
+            else:
+                position = resolution.find('x')
+                width = int(resolution[:position])
+                height = int(resolution[position+1:])
+        else:
+            width = 640
+            height = 480
+        return [width, height]
+
+    def clean_framerate(self):
+        framerate = self.cleaned_data['framerate']
+        if framerate:
+            framerate = float(framerate)
+        if not framerate:
+            framerate = 1.0
+        return framerate
+
+    def clean_pixelformat(self):
+        pixelformat = self.cleaned_data['pixelformat']
+        pixelformat = FORMATS[int(pixelformat)][1]
+        return pixelformat
+
+
     # def clean_hue(self):
     #     hue = self.cleaned_data['hue']
     #     if hue:
@@ -256,39 +400,6 @@ class ShootConfigurationForm(forms.Form):
 
 
 
-            # resolution
-
-            # resolution = forms.CharField(
-            #                 label='Resolution',
-            #                 required=False,
-            #                 max_length = 9,
-            #                 widget=forms.TextInput(
-            #                                     attrs={
-            #                                             'size': '9',
-            #                                             'placeholder':'WIDTHxHEIGHT',
-            #                                             'class':'form-control',
-            #                                             'data-toggle':"tooltip",
-            #                                             'data-placement':'top',
-            #                                             'title':"def. 1024x1024",
-            #                                             }
-            #                                         )
-            #                                     )
-            # framerate = forms.DecimalField(label='Framerate',
-            #                 max_digits=3,
-            #                 required=False,
-            #                 decimal_places=1,
-            #                 max_value=15,
-            #                 min_value=0.1,
-            #                 widget=forms.NumberInput(
-            #                                     attrs={
-            #                                             'class':'form-control',
-            #                                             'data-toggle':'tooltip',
-            #                                             'data-placement':'top',
-            #                                             'placeholder':'1.0',
-            #                                             'title':"E.g. 0.6 or 5",
-            #                                             }
-            #                                         )
-            #                                     )
             # shutter_speed = 0 automatic / its in us / it is limited by the framrate, MUST set before a extrmely slow framrate \ 1/fps es lo mas lento
             # shutter_speed = forms.DecimalField(
             #                     label='Shutter speed',
--- app/detection/views.py
+++ app/detection/views.py
@@ -10,16 +10,25 @@ from django.core.files.storage import FileSystemStorage
 import time
 import subprocess
 
-cam_props = {'brightness': 128, 'contrast': 128, 'saturation': 180,
-             'gain': 0, 'sharpness': 128, 'exposure_auto': 1,
-             'exposure_absolute': 150, 'exposure_auto_priority': 0,
-             'focus_auto': 0, 'focus_absolute': 30, 'zoom_absolute': 250,
-             'white_balance_temperature_auto': 0, 'white_balance_temperature': 3300}
+INITIALS = { 'brightness': 50,
+            'contrast': 0,
+            'saturation': 0,
+            'red_balance': 1000,
+            'blue_balance': 1000,
+            'sharpness': 0,
+            'auto_exposure': '0',
+            'exposure_time_absolute': 1000,
+            'iso_sensitivity': '0',
+            'iso_sensitivity_auto': '0',
+            'resolution':'1280x720',
+            'framerate':'15.0',
+            'pixelformat':'3'}
+
 # Create your views here.
 form={}
 class Capture_View(View):
     def get(self, request):
-        form['form'] = ShootConfigurationForm()
+        form['form'] = ShootConfigurationForm(initial=INITIALS)
         return render(
                         request,
                         "capture.html",
@@ -30,44 +39,65 @@ class Capture_View(View):
         form['form'] = ShootConfigurationForm(request.POST or None)
         if form['form'].is_valid():
             conf = form['form'].cleaned_data
-            print(conf)
+            # devices = subprocess.check_output(['v4l2-ctl','--list-devices']).decode('utf-8').split('\n\t')
+            # name = devices[0][0:].split(':')[0]
+            # description = devices[0][0:].split(':')[1]
+            # # .split('\n\t')
+            # print(a)
 
-            subprocess.call(['v4l2-ctl --list-devices'])
+            width = conf['resolution'][0]
+            height = conf['resolution'][1]
+            framerate = conf['framerate']
+            pixelformat = conf['pixelformat']
+            print(pixelformat)
+            conf.pop('resolution')
+
+            # set resolution
+            subprocess.run(['v4l2-ctl',f'v4l2-ctl --set-fmt-video=width={width}',f'height={height}'],stdout=subprocess.DEVNULL, shell=True)
+            # set framerate
+            subprocess.call(['v4l2-ctl',f'--set-parm={framerate}'],stdout=subprocess.DEVNULL, shell=False)
+            # start format
+            subprocess.call(['v4l2-ctl','--set-fmt-video',f'pixelformat={pixelformat}'],stdout=subprocess.DEVNULL, shell=True)
             for key in conf:
-                subprocess.call([f'v4l2-ctl -d /dev/video0 -c {key}={str(cam_props[key])}'],shell=True)
-            cap = cv2.VideoCapture(0)
+                try:
+                    subprocess.run([f'v4l2-ctl -d /dev/video0 -c {key}={str(conf[key])}'],stdout=subprocess.DEVNULL, shell=True)
+                except KeyError:
+                    print('Error trying to configure. Wrong Camera?')
 
-            # 10. CV_CAP_PROP_BRIGHTNESS Brightness of the image (only for cameras).
-            # 11. CV_CAP_PROP_CONTRAST Contrast of the image (only for cameras).
-            # 12. CV_CAP_PROP_SATURATION Saturation of the image (only for cameras).
-            # 13. CV_CAP_PROP_HUE Hue of the image (only for cameras).
-            # 14. CV_CAP_PROP_GAIN Gain of the image (only for cameras).
-            # 15. CV_CAP_PROP_EXPOSURE Exposure (only for cameras).
+            # Take picture
+            subprocess.call('pwd',shell=True)
+            subprocess.call(['v4l2-ctl','--stream-mmap','--stream-count=1','--stream-skip=3','--stream-to='+'./'+STATIC_ROOT+'/best.jpeg'])
+
+            print(STATIC_ROOT+'/best.jpeg')
+            # cap = cv2.VideoCapture(0)
             # cap.set(5,30)
-            # cap.set(9,0)
             #
-            # # -64,64
-            # cap.set(10,conf['brightness'])
-            # # 0 a 95
-            # cap.set(11,conf['contrast'])
-            # # 0 a 100
-            # cap.set(12,conf['saturation'])
-            # # -2000 y 2000
-            # cap.set(13,conf['hue'])
-            # # 1 a 8
-            # cap.set(14,conf['gain'])
+            # # cap.set(5,30)
+            # # cap.set(9,0)
             # #
-            # cap.set(15,conf['exposure'])
-
-            ret, frame = cap.read()
-
-            cv2.imwrite(filename=STATIC_ROOT+'best.jpg', img=frame)
-            cap.release()
+            # # # -64,64
+            # # cap.set(10,conf['brightness'])
+            # # # 0 a 95
+            # # cap.set(11,conf['contrast'])
+            # # # 0 a 100
+            # # cap.set(12,conf['saturation'])
+            # # # -2000 y 2000
+            # # cap.set(13,conf['hue'])
+            # # # 1 a 8
+            # # cap.set(14,conf['gain'])
+            # # #
+            # # cap.set(15,conf['exposure'])
+            #
+            # ret, frame = cap.read()
+            #
+            # cv2.imwrite(filename=STATIC_ROOT+'best.jpg', img=frame)
+            # cap.release()
 
             fs = FileSystemStorage()
-            photo = STATIC_ROOT+'best.jpg'
+            photo = '.'+STATIC_ROOT+'/best.jpeg'
+            print(photo)
             with open(photo, 'rb') as f:
-                name = fs.save('best.jpg', File(f))
+                name = fs.save('best.jpeg', File(f))
                 print(fs.url(name))
                 # foto = PlatePhoto_Db(name='foto1', photo=File(contents))
                 data = {
