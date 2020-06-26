09295f79e6b66ab927863775d481394b3f1731a1
lucassinggg <lucassinggg@gmail.com>
2020-06-26 12:54:49 +0200

    cleaning process improve

--- app/finecontrol/models.py
+++ app/finecontrol/models.py
@@ -12,3 +12,15 @@ class GcodeFile(models.Model):
     gcode = models.FileField(null=True)
     gcode_url = models.CharField(max_length=100, null=True)
     datetime = models.DateTimeField(auto_now_add=True, null=True)
+
+class CleaningProcess_Db(models.Model):
+    user = models.ForeignKey(
+                get_user_model(),
+                null=True,
+                on_delete=models.CASCADE,
+                blank=True,
+                )
+    start_frequency = models.DecimalField(max_digits=4, decimal_places=0, blank=True, null=True)
+    stop_frequency = models.DecimalField(max_digits=4, decimal_places=0, blank=True, null=True)
+    steps = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    pressure = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
