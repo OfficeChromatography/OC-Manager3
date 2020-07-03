b55afd35ba2193d559be94c873546a1192c13166
lucassinggg <lucassinggg@gmail.com>
2020-07-03 14:20:58 +0200

    Readable samplejs

--- app/templates/sample.js
+++ app/templates/sample.js
@@ -1,3 +1,4 @@
+// Graph var
 var ctx = document.getElementById('plotPreview').getContext('2d');
 var plotPreview = new Chart(ctx, {
    type: 'scatter',
@@ -37,42 +38,51 @@ var plotPreview = new Chart(ctx, {
     }
    },
 });
-var datatable
-// Execute every time something happens wi
+
+// Table constants
+const $tableID = $('#table');
+const $BTN = $('#export-btn');
+const $EXPORT = $('#export');
+var tablevalues
+  // A few jQuery helpers for exporting only
+jQuery.fn.pop = [].pop;
+jQuery.fn.shift = [].shift;
+
+// Execute every time something happens
 $("#id_motor_speed").change(
   function(){
     console.log('motor');
-    loadresume()
+    loadResumeTable()
   }
 )
 $("#id_pressure").change(
   function(){
     console.log('pres');
-    loadresume()
+    loadResumeTable()
   }
 )
 $("#id_frequency").change(
   function(){
     console.log('dpre');
-    loadresume()
+    loadResumeTable()
   }
 )
 $("#id_delta_y").change(
   function(){
     console.log('dey');
-    loadresume()
+    loadResumeTable()
   }
 )
 $("#id_delta_x").change(
   function(){
     console.log('dex');
-    loadresume()
+    loadResumeTable()
   }
 )
-$("#id_main_property").change(
+$("#id_mainCalculations_property").change(
   // The default hiden is in the html file
   function(){
-    switch ($("#id_main_property").val()) {
+    switch ($("#id_mainCalculations_property").val()) {
       case '1':
           $("#id_valuesform").fadeOut();
           $("#id_valuesunit").html('#');
@@ -88,78 +98,78 @@ $("#id_main_property").change(
           $("#nbandsrow").hide();
           $("#lengthbandsrow").show();
         break;
-    loadresume()
+    loadResumeTable()
     }
   }
 )
 
-
 $("#id_size_x").change(
   function(){
     console.log('sizex');
-    changegraphsize()
-    loadresume()
+    changeGraphSize()
+    loadResumeTable()
   }
 )
 $("#id_size_y").change(
   function(){
     console.log('sizey');
-    changegraphsize()
-    loadresume()
+    changeGraphSize()
+    loadResumeTable()
   }
 );
 
 $("#id_offset_left").change(
     function(){
-      bandsmain()
-      loadresume()
+      mainCalculations()
+      loadResumeTable()
     }
 );
 $("#id_offset_right").change(
   function(){
-    bandsmain()
-    loadresume()
+    mainCalculations()
+    loadResumeTable()
   }
 );
 $("#id_offset_bottom").change(
     function(){
-      bandsmain()
-      loadresume()
+      mainCalculations()
+      loadResumeTable()
     }
 );
 $("#id_offset_top").change(
   function(){
-    bandsmain()
-    loadresume()
+    mainCalculations()
+    loadResumeTable()
   }
 );
 
-$("#id_main_property").change(
+$("#id_mainCalculations_property").change(
   function(){
-    bandsmain()
-    loadresume()
+    mainCalculations()
+    loadResumeTable()
   }
 )
 $("#id_value").change(
   function(){
-    bandsmain()
-    loadresume()
+    mainCalculations()
+    loadResumeTable()
   }
 )
 $("#id_height").change(
   function(){
-    bandsmain()
-    loadresume()
+    mainCalculations()
+    loadResumeTable()
   }
 )
 $("#id_gap").change(
   function(){
-    bandsmain()
-    loadresume()
+    mainCalculations()
+    loadResumeTable()
   }
 )
 
-function bandsmain(){
+// MAIN
+function mainCalculations(){
   plate_x_size = parseFloat($("#id_size_x").val());
   plate_y_size = parseFloat($("#id_size_y").val());
 
@@ -178,33 +188,32 @@ function bandsmain(){
   // Check if theres missing parameters
   missing_parameter = (isNaN(plate_x_size)||isNaN(plate_y_size)||isNaN(offset_left_size)||isNaN(offset_right_size)||isNaN(offset_top_size)||isNaN(offset_bottom_size)||isNaN(gap_size)||isNaN(band_height))
 
-  if(theres_error('#id_parameter_error',missing_parameter)){return}
+  if(thereAreErrors('#id_parameter_error',missing_parameter)){return}
 
   // Calculate the Working Area [x,y]
-  working_area = nbands_working_area(plate_x_size,offset_left_size,offset_right_size,plate_y_size,offset_top_size,offset_bottom_size)
+  working_area = nBandsWorkingArea(plate_x_size,offset_left_size,offset_right_size,plate_y_size,offset_top_size,offset_bottom_size)
 
   // Check if its not posible to calculate the wa
-  if(theres_error('#id_offsets_error',isNaN(working_area[0]) && isNaN(working_area[1]))){return}
+  if(thereAreErrors('#id_offsets_error',isNaN(working_area[0]) && isNaN(working_area[1]))){return}
 
   // Check if the vertical sizes is enough
-  if(theres_error('#id_space_error',working_area[1]<band_height)){return}
-
+  if(thereAreErrors('#id_space_error',working_area[1]<band_height)){return}
 
   switch (property) {
+    // N Bands
     case '1':
     //Gap process
-      sum_gaps_size = total_gap_length(number_bands, gap_size)
-      if(theres_error('#id_gap_error',isNaN(sum_gaps_size) || sum_gaps_size>= working_area[0])){return}
+      sum_gaps_size = totalGapLength(number_bands, gap_size)
+      if(thereAreErrors('#id_gap_error',isNaN(sum_gaps_size) || sum_gaps_size>= working_area[0])){return}
 
     //Bands Sizes
-      band_size = total_bands_length(working_area,sum_gaps_size,number_bands)
-      if(theres_error('#id_space_error',isNaN(band_size))){return}
+      band_size = totalBandsLength(working_area,sum_gaps_size,number_bands)
+      if(thereAreErrors('#id_space_error',isNaN(band_size))){return}
       break;
-
-
+    // Length
     case '2':
       number_bands = Math.trunc(working_area[0]/(band_size+gap_size))
-      if(theres_error('#id_space_error',number_bands<1)){return}
+      if(thereAreErrors('#id_space_error',number_bands<1)){return}
       break;
   }
 
@@ -225,32 +234,59 @@ function bandsmain(){
       newdata[3]={y:offset_bottom_size,x:(i+1)*band_size+(gap_size*i)+offset_left_size}
       newdata[4]=newdata[0]
     }
-    addData(plotPreview,i,'black', newdata)
+    addData2Chart(plotPreview,i,'black', newdata)
   }
   plotPreview.update();
-  banddescrition(number_bands);
+  newComponentsTable(number_bands);
 }
 
-function banddescrition(number_row){
-  let newTr1 = `
-  <tr class="hide">
-  <td class="pt-3-half">`;
-  let newTr2 = `</td>
-  <td class="pt-3-half" contenteditable="true"></td>
-  <td class="pt-3-half" contenteditable="true"></td>
-  <td class="pt-3-half" contenteditable="true"></td>
-  </tr>`
-  $('#tbody_band').empty()
-  console.log(number_row);
-  number_row = parseInt(number_row)
-  for(i=0;i<number_row;i++){
-    $('#tbody_band').append(newTr1+(i+1)+newTr2);
+//  ERROR DISPLAY MANAGER
+function thereAreErrors(error_id, bolean_exp){
+  if(bolean_exp){
+    $(error_id).fadeIn();
+    return true
+  }
+  else{
+    $(error_id).fadeOut();
+    return false
   }
 }
 
-function lenghtcalc(){}
+//  Calculates the Working Area
+function nBandsWorkingArea(plate_x_size,offset_left_size,offset_right_size,plate_y_size,offset_top_size,offset_bottom_size){
+  working_area = [plate_x_size-offset_left_size-offset_right_size,plate_y_size-offset_top_size-offset_bottom_size]
+  if(working_area[0] <= 0 || working_area[1] <= 0 || isNaN(working_area[0]) || isNaN(working_area[1])){
+    return [NaN,NaN];
+  }
+  else{
+      return working_area;
+  }
+}
 
-function addData(chart, label, color, data) {
+//  Calculate the sum of gaps lenght
+function totalGapLength(number_bands, gap_size){
+  number_of_gaps = number_bands - 1;
+  if(number_of_gaps<0){
+    return NaN
+  }
+  else{
+    return gap_size*number_of_gaps;
+  }
+}
+
+//  Calculate the sum of bands lenght
+function totalBandsLength(working_area,sum_gaps_size,number_bands){
+  bands_size = (working_area[0]-sum_gaps_size)/number_bands
+  if(bands_size<=0){
+    return NaN
+  }
+  else{
+    return bands_size
+  }
+}
+
+// add new bands to the chart
+function addData2Chart(chart, label, color, data) {
 		chart.data.datasets.push({
 	    label: label,
       backgroundColor: color,
@@ -266,54 +302,13 @@ function addData(chart, label, color, data) {
     chart.update();
 }
 
-//Calculates the Working Area
-function nbands_working_area(plate_x_size,offset_left_size,offset_right_size,plate_y_size,offset_top_size,offset_bottom_size){
-  working_area = [plate_x_size-offset_left_size-offset_right_size,plate_y_size-offset_top_size-offset_bottom_size]
-  if(working_area[0] <= 0 || working_area[1] <= 0 || isNaN(working_area[0]) || isNaN(working_area[1])){
-    return [NaN,NaN];
-  }
-  else{
-      return working_area;
-  }
-}
-
-function total_gap_length(number_bands, gap_size){
-  number_of_gaps = number_bands - 1;
-  if(number_of_gaps<0){
-    return NaN
-  }
-  else{
-    return gap_size*number_of_gaps;
-  }
-}
-
-function total_bands_length(working_area,sum_gaps_size,number_bands){
-  bands_size = (working_area[0]-sum_gaps_size)/number_bands
-  if(bands_size<=0){
-    return NaN
-  }
-  else{
-    return bands_size
-  }
-}
-
-function theres_error(error_id, bolean_exp){
-  if(bolean_exp){
-    $(error_id).fadeIn();
-    return true
-  }
-  else{
-    $(error_id).fadeOut();
-    return false
-  }
-}
-
-function loadresume(){
+// load resume table with fields data
+function loadResumeTable(){
   $('#motorspeed_resume').text($("#id_motor_speed").val())
   $('#appconst_resume').text($("#id_pressure").val()+','+$("#id_frequency").val())
   $('#sizes_resume').text($("#id_size_x").val()+','+$("#id_size_y").val())
   $('#offsets_resume').text($("#id_offset_left").val()+','+$("#id_offset_right").val()+','+$("#id_offset_top").val()+','+$("#id_offset_bottom").val())
-  $('#band_properties_resume').text($("#id_main_property option:selected").text())
+  $('#band_properties_resume').text($("#id_mainCalculations_property option:selected").text())
   $('#n_bands_resume').text($("#id_value").val())
   $('#length_resume').text($("#id_value").val())
   $('#delta_resume').text($("#id_delta_x").val()+','+$("#id_delta_y").val())
@@ -321,7 +316,25 @@ function loadresume(){
   $('#gap_resume').text($("#id_gap").val())
 }
 
-function loadtable(band,fromDB){
+// Create a new Table with a given number of rows
+function newComponentsTable(number_row){
+  let newTr1 = `
+  <tr class="hide">
+  <td class="pt-3-half">`;
+  let newTr2 = `</td>
+  <td class="pt-3-half" contenteditable="true"></td>
+  <td class="pt-3-half" contenteditable="true"></td>
+  <td class="pt-3-half" contenteditable="true"></td>
+  </tr>`
+  $('#tbody_band').empty()
+  console.log(number_row);
+  number_row = parseInt(number_row)
+  for(i=0;i<number_row;i++){
+    $('#tbody_band').append(newTr1+(i+1)+newTr2);
+  }
+}
+// Load the table with data from de DB or from file
+function loadComponentsTable(band,fromDB){
   console.log(band);
   var newTr1
   $('#tbody_band').empty()
@@ -345,12 +358,118 @@ function loadtable(band,fromDB){
   }
 }
 
-function changegraphsize(){
+// Load field values with 'data' Object
+function loadFieldsValues(data){
+  $("#id_motor_speed").val(data.motor_speed)
+  $("#id_pressure").val(data.pressure)
+  $("#id_frequency").val(data.frequency)
+  $("#id_temperature").val(data.temperature)
+  $("#id_delta_y").val(data.delta_y)
+  $("#id_delta_x").val(data.delta_x)
+
+
+  $("#id_size_x").val(data.size_x)
+  $("#id_size_y").val(data.size_y)
+
+  $("#id_offset_left").val(data.offset_left)
+  $("#id_offset_right").val(data.offset_right)
+  $("#id_offset_top").val(data.offset_top)
+  $("#id_offset_bottom").val(data.offset_bottom)
+
+  $("#id_mainCalculations_property").val(data.mainCalculations_property)
+  $("#id_value").val(data.value)
+  $("#id_height").val(data.height)
+  $("#id_gap").val(data.gap)
+
+  $( "#id_value" ).trigger( "change" );
+  $('#id_load_sucess').html(data.file_name+' successfully load!')
+  $( "#id_load_sucess" ).fadeIn().delay( 800 ).fadeOut( 400 );
+}
+
+// Returns a string or an Object with the table values
+function getTableValues(toString){
+
+ const $rows = $tableID.find('tr:not(:hidden)');
+ const headers = [];
+ const data = [];
+
+ // Get the headers (add special header logic here)
+ $($rows.shift()).find('th:not(:empty)').each(function () {
+
+   headers.push($(this).text().toLowerCase());
+ });
+
+ // Turn all existing rows into a loopable array
+ $rows.each(function () {
+   const $td = $(this).find('td');
+   const h = {};
+
+   // Use the headers from earlier to name our hash keys
+   headers.forEach((header, i) => {
+
+     h[header] = $td.eq(i).text();
+   });
+
+   data.push(h);
+ });
+
+ // Output the result
+ if(toString){
+   tablevalues = '&table='+JSON.stringify(data)
+ }
+ else{
+   tablevalues = data
+ }
+ return tablevalues
+};
+
+// Change the Graph sizes with the size x and y field values.
+function changeGraphSize(){
   plotPreview.config.options.scales.xAxes[0].ticks.max = parseFloat($("#id_size_x").val());
   plotPreview.config.options.scales.yAxes[0].ticks.max = parseFloat($("#id_size_y").val());
   plotPreview.update();
 }
 
+// Return form data as Object
+function getFormData($form){
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function(n, i){
+        indexed_array[n['name']] = n['value'];
+    });
+
+    return indexed_array;
+}
+
+// Method that control the file load
+function getAsText(readFile) {
+
+  var reader = new FileReader();
+
+  // Read file into memory as UTF-16
+  reader.readAsText(readFile, "UTF-8");
+
+  // Handle progress, success, and errors
+  reader.onload = loaded;
+  reader.onerror = errorHandler;
+
+  function loaded(evt) {
+    var fileString = evt.target.result;
+    console.log(fileString);
+    jsonObject = JSON.parse(fileString)
+    loadFieldsValues(jsonObject)
+    loadComponentsTable(jsonObject['bands'],false)
+    changeGraphSize()
+  }
+  function errorHandler(evt) {
+    if(evt.target.error.name == "NotReadableError") {
+      // The file could not be read
+    }
+  }
+}
+
+// Endpoints
 $('#stopbttn').on('click', function (e) {
   event.preventDefault()
   //
@@ -392,7 +511,7 @@ $('#startbttn').on('click', function (e) {
 })
 $('#savebttn').on('click', function (e) {
   event.preventDefault()
-  $formData = $('#plateform').serialize()+'&'+$('#movementform').serialize()+'&'+$('#saveform').serialize()+gettablevalues(true)
+  $formData = $('#plateform').serialize()+'&'+$('#movementform').serialize()+'&'+$('#saveform').serialize()+getTableValues(true)
   $endpoint = window.location.origin+'/samplesave/'
   $.ajax({
   method: 'POST',
@@ -429,7 +548,6 @@ $('#hommingbttn').on('click', function (e) {
   })
 })
 
-
 // Import/Export DATA
 $('#downloadfilebttn').on('click', function (e) {
   event.preventDefault()
@@ -437,7 +555,7 @@ $('#downloadfilebttn').on('click', function (e) {
 
   var plate = getFormData($('#plateform'))
   var movement = getFormData($('#movementform'))
-  var table = {bands:gettablevalues(false)}
+  var table = {bands:getTableValues(false)}
   items = Object.assign(plate,movement,table)
 
   content = JSON.stringify(items);
@@ -466,49 +584,13 @@ $('#file').on('change',function(e){
                 $(this).next('.custom-file-label').html(fileName.name);
             })
 
-function getFormData($form){
-    var unindexed_array = $form.serializeArray();
-    var indexed_array = {};
-
-    $.map(unindexed_array, function(n, i){
-        indexed_array[n['name']] = n['value'];
-    });
-
-    return indexed_array;
-}
-function getAsText(readFile) {
-
-  var reader = new FileReader();
-
-  // Read file into memory as UTF-16
-  reader.readAsText(readFile, "UTF-8");
-
-  // Handle progress, success, and errors
-  reader.onload = loaded;
-  reader.onerror = errorHandler;
-}
-function loaded(evt) {
-  var fileString = evt.target.result;
-  console.log(fileString);
-  jsonObject = JSON.parse(fileString)
-  loadformvalues(jsonObject)
-  loadtable(jsonObject['bands'],false)
-  changegraphsize()
-}
-function errorHandler(evt) {
-  if(evt.target.error.name == "NotReadableError") {
-    // The file could not be read
-  }
-}
-
-
 
 function hommingMethodSuccess(data, textStatus, jqXHR){
   if(data.error!=''){
-    theres_error('#id_parameter_error',false)
+    thereAreErrors('#id_parameter_error',false)
   }
   else{
-    theres_error('#id_parameter_error',true)
+    thereAreErrors('#id_parameter_error',true)
   }
 
 }
@@ -537,9 +619,9 @@ function startMethodError(jqXHR, textStatus, errorThrown){}
 
 function loadMethodSuccess(data, textStatus, jqXHR){
   // Load all the fields with the ones get in the database
-  loadformvalues(data);
-  loadtable(data['bands'],true)
-  changegraphsize()
+  loadFieldsValues(data);
+  loadComponentsTable(data['bands'],true)
+  changeGraphSize()
 }
 function loadMethodError(jqXHR, textStatus, errorThrown){
   console.log('error');
@@ -563,76 +645,3 @@ function saveMethodError(jqXHR, textStatus, errorThrown){
   $('#id_save_error').html(data.error)
   $( "#id_save_error" ).fadeIn().delay( 800 ).fadeOut( 400 );
 }
-
-function loadformvalues(data){
-  $("#id_motor_speed").val(data.motor_speed)
-  $("#id_pressure").val(data.pressure)
-  $("#id_frequency").val(data.frequency)
-  $("#id_temperature").val(data.temperature)
-  $("#id_delta_y").val(data.delta_y)
-  $("#id_delta_x").val(data.delta_x)
-
-
-  $("#id_size_x").val(data.size_x)
-  $("#id_size_y").val(data.size_y)
-
-  $("#id_offset_left").val(data.offset_left)
-  $("#id_offset_right").val(data.offset_right)
-  $("#id_offset_top").val(data.offset_top)
-  $("#id_offset_bottom").val(data.offset_bottom)
-
-  $("#id_main_property").val(data.main_property)
-  $("#id_value").val(data.value)
-  $("#id_height").val(data.height)
-  $("#id_gap").val(data.gap)
-
-  $( "#id_value" ).trigger( "change" );
-  $('#id_load_sucess').html(data.file_name+' successfully load!')
-  $( "#id_load_sucess" ).fadeIn().delay( 800 ).fadeOut( 400 );
-}
-
-// TABLE FUNCTIONS
-const $tableID = $('#table');
-const $BTN = $('#export-btn');
-const $EXPORT = $('#export');
-var tablevalues
-
-// A few jQuery helpers for exporting only
-jQuery.fn.pop = [].pop;
-jQuery.fn.shift = [].shift;
-
-function gettablevalues(toString){
-
- const $rows = $tableID.find('tr:not(:hidden)');
- const headers = [];
- const data = [];
-
- // Get the headers (add special header logic here)
- $($rows.shift()).find('th:not(:empty)').each(function () {
-
-   headers.push($(this).text().toLowerCase());
- });
-
- // Turn all existing rows into a loopable array
- $rows.each(function () {
-   const $td = $(this).find('td');
-   const h = {};
-
-   // Use the headers from earlier to name our hash keys
-   headers.forEach((header, i) => {
-
-     h[header] = $td.eq(i).text();
-   });
-
-   data.push(h);
- });
-
- // Output the result
- if(toString){
-   tablevalues = '&table='+JSON.stringify(data)
- }
- else{
-   tablevalues = data
- }
- return tablevalues
-};
