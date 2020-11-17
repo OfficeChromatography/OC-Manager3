03292eae40c15809ed2e83bd5dd67bd3b8c95335
Marcelknapp <marcelknapp1992@gmail.com>
2020-11-17 12:38:01 +0100

    added nozzle control to fine control

---
+++ app/templates/modules/nozzleControl/nozzleControl.js
@@ -0,0 +1,59 @@
+var zero_position = 0
+$('#speedrangeN').on('change',function(){
+  $('#speedtextN').val($(this).val())
+})
+$('#speedtextN').on('change',function(){
+  $('#speedrangeN').val($(this).val())
+})
+
+$('#steprangeN').on('change',function(){
+  $('#steptextN').val($(this).val())
+})
+$('#steptextN').on('change',function(){
+  $('#steprangeN').val($(this).val())
+})
+$('#up_arrowN').on('click',function(){
+  gcode = movement('E-')
+  sendToMachine(gcode)
+})
+$('#down_arrowN').on('click',function(){
+  gcode = movement('E')
+  sendToMachine(gcode)
+})
+$('#hommingN').on('click',function(){
+  gcode = 'G41'
+  sendToMachine(gcode)
+  gcode = 'G92E0'
+  sendToMachine(gcode)
+  zero_position = 0
+  $('#currentPositionN').text('e: ' + zero_position)
+})
+
+
+function movement(direction){
+  mm = $('#steptextN').val()
+  value = 'G0'+direction+$('#steptextN').val()
+  if(direction.includes("-")){
+    zero_position[0]=parseInt(zero_position[0])-parseInt(mm);
+  }
+  else{
+    zero_position[0]+=parseInt(mm);
+  }
+
+  $('#currentPositionN').text('x: ' + zero_position)
+  return value;
+}
+
+function sendToMachine(value){
+  data={'gcode':value}
+  console.log(data);
+  $.ajax({
+    method: 'POST',
+    url:    window.location.origin+'/send/',
+    data:   data,
+    success: setHommingEndpointSucess,
+    error: setHommingEndpointError,
+  })
+  function setHommingEndpointSucess(data, textStatus, jqXHR){}
+  function setHommingEndpointError(jqXHR, textStatus, errorThrown){}
+}
