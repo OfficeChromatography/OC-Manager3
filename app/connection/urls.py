940aa1c58f8fa60d299dd72116ba1f4a4a8da4f8
lucassinggg <lucassinggg@gmail.com>
2020-11-18 10:34:31 +0100

    Changed imports in connections.urls

--- app/connection/urls.py
+++ app/connection/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from connection.views import Connection_test, IsConnected, CommunicationWithOC
+from connection.views import *
 urlpatterns = [
     path('connection/', Connection_test.as_view(), name='connection'),
     path('isconnected/', IsConnected.as_view(), name='isconnected'),
