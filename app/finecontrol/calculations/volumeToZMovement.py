121270ae5b4c0d34767bd962e4d5e81fa9de26ec
Marcelknapp <marcelknapp1992@gmail.com>
2020-11-24 12:59:21 +0100

    fixed bug

--- app/finecontrol/calculations/volumeToZMovement.py
+++ app/finecontrol/calculations/volumeToZMovement.py
@@ -2,7 +2,7 @@ def volumeToZMovement(volume):
     '''
     volume in ml -> zMovement in (mm)
     '''
-    return round(30*volume/2000,2)
+    return round(30*float(volume)/2000,2)
 
 def zMovementToVolume(zMovement):
-    return round(zMovement*2000/30,2)
+    return round(float(zMovement)*2000/30,2)
