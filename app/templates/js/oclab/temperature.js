7298717f81a94ed497886f756145c1d481adb7cd
Marcelknapp <marcelknapp1992@gmail.com>
2020-11-24 13:06:35 +0100

    deleted starttemp js

--- app/templates/js/oclab/temperature.js
+++ app/templates/js/oclab/temperature.js
@@ -1,19 +1,4 @@
 
-$("#start_temp").on('click',function(e){
-    event.preventDefault();
-    console.log($("#temp-form").serialize());
-    $.ajax({
-    method: 'POST',
-    url:    window.location.origin+'/tempControl/',
-    data:   $("#temp-form").serialize(),
-    success: staticCleanMethodSuccess,
-    error: staticCleanMethodError,
-    })
-    function staticCleanMethodSuccess(data, textStatus, jqXHR){
-    }
-    function staticCleanMethodError(jqXHR, textStatus, errorThrown){}
-    })
-
 $("#tempOnButton").on('click',function(e){
     event.preventDefault();
     console.log($("#temp-form").serialize()+'&active=On');
