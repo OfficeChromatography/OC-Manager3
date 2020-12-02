30217e04086611d285578631193643f410125880
Marcelknapp <marcelknapp1992@gmail.com>
2020-12-02 11:09:12 +0100

    changed the sampleapp

--- app/finecontrol/calculations/sampleAppCalc.py
+++ app/finecontrol/calculations/sampleAppCalc.py
@@ -42,34 +42,27 @@ from finecontrol.calculations.flowCalc import FlowCalc
 #     return [deltaX[0], realVolume]
 
 def returnDropEstimateVol(data):
-    #print(data)
-    working_area = [float(data.size_x[0])-float(data.offset_left[0])-float(data.offset_right[0]),float(data.size_y[0])-float(data.offset_top[0])-float(data.offset_bottom[0])]
-    if int(data.main_property[0])==1:
-        n_bands = int(data.value[0])
+    working_area = [float(data.size_x)-float(data.offset_left)-float(data.offset_right)
+    ,float(data.size_y)-float(data.offset_top)-float(data.offset_bottom)]
+    if int(data.main_property)==1:
+        n_bands = int(data.value)
         number_of_gaps = n_bands - 1
-        sum_gaps_size = float(data.gap[0])*number_of_gaps
+        sum_gaps_size = float(data.gap)*number_of_gaps
         length = (working_area[0]-sum_gaps_size)/n_bands
     else:
-        length = data.value[0]
-        n_bands = int(math.trunc(working_area[0]/(float(length)+float(data.gap[0]))))
+        length = data.value
+        n_bands = int(math.trunc(working_area[0]/(float(length)+float(data.gap))))
         
-
-    dataTable = json.loads(data.table[0])
     results = []
-    for table in dataTable:
+    for table in data.table:
 
-        dropVolume = FlowCalc(pressure=float(data.pressure[0]), nozzleDiameter=data.nozzlediameter[0], timeOrFrequency = float(data.frequency[0]), fluid=table['type'], density=table['density'], viscosity=table['viscosity']).calcVolumeFrequency()
+        dropVolume = FlowCalc(pressure=float(data.pressure), nozzleDiameter=data.nozzlediameter, timeOrFrequency = float(data.frequency), fluid=table['type'], density=table['density'], viscosity=table['viscosity']).calcVolumeFrequency()
 
-        pointsX = np.round(float(length)/float(data.delta_x[0]))+1
-        print(length)
-        pointsY = 1
-        vol2 = (pointsX-1) * dropVolume
-        if data.height[0] != "0":
-            pointsY = np.round(float(data.height[0])/float(data.delta_y[0]))+1
-            vol2 = (pointsX-1) * (pointsY-1) * dropVolume
+        pointsX = np.round(float(length)/float(data.delta_x))+1
+        pointsY = np.round(float(data.height)/float(data.delta_y))+1
+        vol2 = (pointsX-1) * (pointsY-1) * dropVolume
         vol = pointsX * pointsY * dropVolume
-        print(vol,vol2)
-        
+        #print(vol,vol2)
         
         volPerBand = (table['volume (ul)'])
         if (volPerBand == "" or volPerBand == "null"):
@@ -94,12 +87,8 @@ def returnDropEstimateVol(data):
             if abs(dif)>vol2/2: 
                 times -= 1
                 realVolume -= vol2
-            
-
-        # times = round(float(volPerBand) / realVolume)
-        # realVolume *= times
-
-        #np.append(results, [dropVolume, realVolume])
+        
+   
         results.append([dropVolume, realVolume, times])
 
     return results
