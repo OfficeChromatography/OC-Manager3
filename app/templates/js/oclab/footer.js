bb187c9ac5fa8253f4fab6400ec7b8cfb4bb9b53
lucassinggg <lucassinggg@gmail.com>
2021-01-04 10:25:12 +0100

    Uncomment code

--- app/templates/js/oclab/footer.js
+++ app/templates/js/oclab/footer.js
@@ -1,34 +1,34 @@
-//$(document).ready(function(){
-//    checkConnection();
-//})
-//
-//function footerState(data){
-//    foot = $('#footer')
-//    footText = $('#footerText')
-//    if (data.connected==true){
-//      $('#footer').removeClass("bg-warning").addClass("bg-success");
-//      footText.html("OC-Lab Connected to "+ data.port)
-//    } else{
-//      foot.removeClass("bg-success").addClass("bg-warning");
-//      footText.html("OC-Lab Disconnected")
-//    }
-//}
-//
-//function checkConnection(){
-////This promise will resolve when the network call succeeds
-//var networkPromise = fetch(window.location.origin + '/isconnected/')
-//                    .then(response=>response.json())
-//                    .then(data=>footerState(data));
-//
-////This promise will resolve when 2 seconds have passed
-//var timeOutPromise = new Promise(function(resolve, reject) {
-//  // 4 Second delay
-//  setTimeout(resolve, 4000, 'Timeout Done');
-//});
-//
-//Promise.all(
-//[networkPromise, timeOutPromise]).then(function(values) {
-//  checkConnection();
-//});
-//}
+$(document).ready(function(){
+    checkConnection();
+})
+
+function footerState(data){
+    foot = $('#footer')
+    footText = $('#footerText')
+    if (data.connected==true){
+      $('#footer').removeClass("bg-warning").addClass("bg-success");
+      footText.html("OC-Lab Connected to "+ data.port)
+    } else{
+      foot.removeClass("bg-success").addClass("bg-warning");
+      footText.html("OC-Lab Disconnected")
+    }
+}
+
+function checkConnection(){
+//This promise will resolve when the network call succeeds
+var networkPromise = fetch(window.location.origin + '/isconnected/')
+                    .then(response=>response.json())
+                    .then(data=>footerState(data));
+
+//This promise will resolve when 2 seconds have passed
+var timeOutPromise = new Promise(function(resolve, reject) {
+  // 4 Second delay
+  setTimeout(resolve, 4000, 'Timeout Done');
+});
+
+Promise.all(
+[networkPromise, timeOutPromise]).then(function(values) {
+  checkConnection();
+});
+}
 
