8c09b120caac2d00f473ede97a960df3ab78160b
lucassinggg <lucassinggg@gmail.com>
2021-02-10 11:06:42 +0100

    First try code decoupling

--- app/finecontrol/calculations/sampleAppCalc.py
+++ app/finecontrol/calculations/sampleAppCalc.py
@@ -7,124 +7,152 @@ from types import SimpleNamespace
 from finecontrol.gcode.GcodeGenerator import GcodeGenerator
 
 
-def returnDropEstimateVol(data):
-    working_area = [float(data.size_x)-float(data.offset_left)-float(data.offset_right)
-    ,float(data.size_y)-float(data.offset_top)-float(data.offset_bottom)]
-    if int(data.main_property)==1:
-        n_bands = int(data.value)
-        number_of_gaps = n_bands - 1
-        sum_gaps_size = float(data.gap)*number_of_gaps
-        length = (working_area[0]-sum_gaps_size)/n_bands
+def calculate_drop_estimated_volume(data):
+    working_area = calculate_working_area(data)
+
+    if int(data.main_property) == 1:
+        nbands, length = precalculations_when_nbands_option_selected(data, working_area[0])
     else:
-        length = data.value
-        n_bands = int(math.trunc(working_area[0]/(float(length)+float(data.gap))))
-        
+        nbands, length = precalculations_when_length_option_selected(data, working_area[0])
+
     results = []
     for table in data.table:
 
-        dropVolume = FlowCalc(pressure=float(data.pressure), nozzleDiameter=data.nozzlediameter, timeOrFrequency = float(data.frequency), fluid=table['type'], density=table['density'], viscosity=table['viscosity']).calcVolumeFrequency()
+        drop_volume = FlowCalc(pressure=float(data.pressure), nozzleDiameter=data.nozzlediameter,
+                              timeOrFrequency=float(data.frequency), fluid=table['type'], density=table['density'],
+                              viscosity=table['viscosity']).calcVolumeFrequency()
 
-        pointsX = np.round(float(length)/float(data.delta_x))+1
-        pointsY = np.round(float(data.height)/float(data.delta_y))+1
-        vol2 = (pointsX-1) * (pointsY-1) * dropVolume
-        vol = pointsX * pointsY * dropVolume
-        #print(vol,vol2)
-        
-        volPerBand = (table['volume'])
-        if (volPerBand == "" or volPerBand == "null"):
-            volPerBand = 0
-        volPerBand = float(volPerBand)
+        x_number_of_points = calculate_number_of_points(length, data.delta_x)
+        y_number_of_points = calculate_number_of_points(data.height, data.delta_y)
 
-        times = 0
-        realVolume = 0
-        dif = volPerBand - realVolume
-        while dif >= 0:
-            if times % 2:
-                realVolume += vol2
-            else:
-                realVolume += vol
-            dif = volPerBand - realVolume
-            times += 1
-        if times % 2:
-            if abs(dif)>vol/2: 
-                times -= 1
-                realVolume -= vol
-        else:
-            if abs(dif)>vol2/2: 
-                times -= 1
-                realVolume -= vol2
-        
-        values = {  "estimated_volume": realVolume,
-                    "estimated_drop_volume": dropVolume,
-                    "times": times,
-                    "minimum_volume": vol}
+        vol2 = (x_number_of_points - 1) * (y_number_of_points - 1) * drop_volume
+        vol = y_number_of_points * y_number_of_points * drop_volume
+        # print(vol,vol2)
+
+        # THIS GOES IN THE CLEAN FORM NOT HERE
+        volume_per_band = (table['volume'])
+        if volume_per_band == "" or volume_per_band == "null":
+            volume_per_band = 0
+        volume_per_band = float(volume_per_band)
+
+        times_to_apply, real_volume = calculate_number_of_times_to_apply(volume_per_band, vol, vol2)
+
+        values = {"estimated_volume": real_volume,
+                  "estimated_drop_volume": drop_volume,
+                  "times": times_to_apply,
+                  "minimum_volume": vol}
         results.append(values)
     return results
 
+
 def minusOneUntilZero(number):
-    number = number -1
+    number = number - 1
     if number < 0: number = 0
     return number
 
 
-def calculate(data):
+def calculate_number_of_times_to_apply(volume_per_band, vol, vol2):
+    times_to_apply = 0
+    real_volume = 0
+    dif = volume_per_band - real_volume
+    while dif >= 0:
+        if times_to_apply % 2:
+            real_volume += vol2
+        else:
+            real_volume += vol
+        dif = volume_per_band - real_volume
+        times_to_apply += 1
 
+    if times_to_apply % 2:
+        if abs(dif) > vol / 2:
+            times_to_apply -= 1
+            real_volume -= vol
+    else:
+        if abs(dif) > vol2 / 2:
+            times_to_apply -= 1
+            real_volume -= vol2
+    return times_to_apply, real_volume
+
+
+def calculate_number_of_points(length, distance_between_points):
+    number_of_points = int(length/distance_between_points) + 1
+    return number_of_points
+
+
+def calculate_working_area(data):
+    x_working_area = data.size_x - data.offset_left - data.offset_right
+    y_working_area = data.size_y - data.offset_top - data.offset_bottom
+    return [x_working_area, y_working_area]
+
+
+def precalculations_when_nbands_option_selected(data, x_working_area):
+    n_bands = int(data.value)
+    number_of_gaps = n_bands - 1
+    sum_gaps_size = data.gap * number_of_gaps
+    length = (x_working_area - sum_gaps_size) / n_bands
+    return n_bands, length
+
+
+def precalculations_when_length_option_selected(data, x_working_area):
+    length = data.value
+    n_bands = int(math.trunc(x_working_area / (length + data.gap)))
+    return n_bands, length
+
+
+def calculate(data):
     data = SimpleNamespace(**data)
 
-    working_area = [data.size_x-data.offset_left-data.offset_right,data.size_y-data.offset_top-data.offset_bottom]
+    working_area = calculate_working_area(data)
 
-    if data.main_property==1:
-        n_bands = int(data.value)
-        number_of_gaps = n_bands - 1
-        sum_gaps_size = data.gap*number_of_gaps
-        length = (working_area[0]-sum_gaps_size)/n_bands
+    if int(data.main_property) == 1:
+        n_bands, length = precalculations_when_nbands_option_selected(data, working_area[0])
     else:
-        length = data.value
-        n_bands = int(math.trunc(working_area[0]/(length+data.gap)))
+        n_bands, length = precalculations_when_length_option_selected(data, working_area[0])
 
+    volume_estimated = calculate_drop_estimated_volume(data)
 
-    volEstimate = returnDropEstimateVol(data)
-    #print(returnDropEstimateVol(data))
-    sampleTimes = [d['times'] for d in volEstimate]
-    
-    
+    sampleTimes = [data_band['times'] for data_band in volume_estimated]
+
     list_of_bands = []
 
     deltaX = float(data.delta_x)
     deltaY = float(data.delta_y)
     j = 0
-    while sum(sampleTimes)!=0:
-        for i in range(0,n_bands):
-            if sampleTimes[i]==0: continue
+    while sum(sampleTimes) != 0:
+        for i in range(0, n_bands):
+            if sampleTimes[i] == 0: continue
             bandlist = []
-            zeros=(i*(length+data.gap))+data.offset_left
+            zeros = (i * (length + data.gap)) + data.offset_left
             if j % 2:
-                current_height = deltaY/2
+                current_height = deltaY / 2
                 while current_height <= data.height:
-                    applicationline=[]
-                    current_length=deltaX/2
-                    while current_length<=length:
-                        applicationline.append([current_length+float(zeros), float(data.offset_bottom)+current_height])
-                        current_length+=deltaX
+                    applicationline = []
+                    current_length = deltaX / 2
+                    while current_length <= length:
+                        applicationline.append(
+                            [current_length + float(zeros), float(data.offset_bottom) + current_height])
+                        current_length += deltaX
                     bandlist.append(applicationline)
-                    current_height+=deltaY
+                    current_height += deltaY
             else:
                 current_height = 0.
                 while current_height <= data.height:
-                    applicationline=[]
-                    current_length=0.
-                    while current_length<=length:
-                        applicationline.append([current_length+float(zeros), float(data.offset_bottom)+current_height])
-                        current_length+=deltaX
+                    applicationline = []
+                    current_length = 0.
+                    while current_length <= length:
+                        applicationline.append(
+                            [current_length + float(zeros), float(data.offset_bottom) + current_height])
+                        current_length += deltaX
                     bandlist.append(applicationline)
-                    current_height+=deltaY
+                    current_height += deltaY
             list_of_bands.append(bandlist)
         j += 1
-        sampleTimes = list(map(minusOneUntilZero,sampleTimes))
-        #print(sampleTimes)
+        sampleTimes = list(map(minusOneUntilZero, sampleTimes))
+        # print(sampleTimes)
 
     # Creates the Gcode for the application and return it
-    return gcode_generation(list_of_bands, data.motor_speed, data.frequency, data.temperature, data.pressure, [data.zero_x,data.zero_y])
+    return gcode_generation(list_of_bands, data.motor_speed, data.frequency, data.temperature, data.pressure,
+                            [data.zero_x, data.zero_y])
 
 
 def gcode_generation(list_of_bands, speed, frequency, temperature, pressure, zeroPosition):
@@ -141,7 +169,7 @@ def gcode_generation(list_of_bands, speed, frequency, temperature, pressure, zer
 
     # Application
     # generate.pressurize(pressure)
-    
+
     generate.rinsing()
     generate.set_new_zero_position(zeroPosition[0], zeroPosition[1], speed)
     jj = 0
@@ -158,11 +186,11 @@ def gcode_generation(list_of_bands, speed, frequency, temperature, pressure, zer
                 generate.open_valve(frequency)
                 generate.finish_moves()
                 jj += 1
-    #Stop heating
-    if (temperature !=0):
+    # Stop heating
+    if (temperature != 0):
         generate.hold_bed_temperature(0)
         generate.report_bed_temperature(0)
-    #Homming
+    # Homming
     generate.homming("XY")
-    #print(generate.list_of_gcodes)
+    # print(generate.list_of_gcodes)
     return generate.list_of_gcodes
