0a696976d4063b6d27eda124a3b84f442b8f069e
lucassinggg <lucassinggg@gmail.com>
2021-05-25 05:51:33 -0300

    docker run by arch

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import subprocess
+import os
 subprocess.run(["sudo", "docker-compose", "pause"], stderr=subprocess.PIPE)
 subprocess.run(["sudo", "docker-compose", "unpause"], stderr=subprocess.PIPE)
-subprocess.run(["sudo", "docker-compose", "up"])
+subprocess.run(["sudo", f"ARCH={os.getenv('SNAP_ARCH')}","docker-compose", "up"])
 
