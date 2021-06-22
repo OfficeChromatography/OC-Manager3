d163156a497c6e3c21b1ba77740b57d964a32371
lucassinggg <lucassinggg@gmail.com>
2021-06-22 10:08:05 -0300

    Air sensor communication

--- app/templates/modules/temperaturehumiditysensor/temperaturehumiditysensor.js
+++ app/templates/modules/temperaturehumiditysensor/temperaturehumiditysensor.js
@@ -1,8 +1,25 @@
+var roomName = 'air_sensor';
+var chatSocket = new WebSocket('ws://' + window.location.host + '/ws/monitor/' + roomName + '/');
+
+chatSocket.onmessage = function(e) {
+    var data = JSON.parse(e.data);
+    $("#air_sensor_module").show()
+    if(data.message!=undefined && data.message.temperature!="0.00" && data.message.humidity!="0.00"){
+        console.log(data.message)
+        $("#air_temperature").text(data.message.temperature)
+        $("#air_humidity").text(data.message.humidity)
+    }
+    else{
+        $("#air_sensor_module").hide()
+    }
+};
+
+chatSocket.onclose = function(e) {
+    $("#air_sensor_module").hide()
+};
+
 getAirSensor = () => {
-    $.get( window.location.origin+"/oclab/airsensor/", function(data) {
-      $("#air_temperature").text(data.temperature+"°C")
-      $("#air_humidity").text(data.humidity+"%")
-    })
+    $.post( window.location.origin+'/send/',{gcode:"G96"})
 }
 
 $("#air_sensor_sync").on("click", getAirSensor)
