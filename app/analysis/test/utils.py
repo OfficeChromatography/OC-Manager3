443cdd2a9c41421bdd1464412163c326193fa8d3
lucassinggg <lucassinggg@gmail.com>
2021-07-23 05:37:06 -0300

    eliminate app

--- app/analysis/test/utils.py
+++
@@ -1,8 +0,0 @@
-import glob
-import os
-
-IMAGE_PATH = os.path.dirname(os.path.abspath(__file__)) + '/test_image.png'
-
-def remove_files_from_local(path):
-    for i in glob.glob(path):
-        os.remove(i)
